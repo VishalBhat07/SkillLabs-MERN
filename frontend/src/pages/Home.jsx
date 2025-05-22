a4b0ee6afc6658953e5de98de971483ab13216f4
VishalBhat07 <vishalbhat21092005@gmail.com>
2025-05-22 16:25:24 +0530

    ..

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,22 @@
-import React, { useEffect } from 'react'
-import axios from "axios"
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
   useEffect(() => {
-   try {
-    
-   } catch (error) {
-    
-   }
-  }, [])
-  return (
-    <div>This is my home</div>
-  )
-}
+    async function getData() {
+      try {
+        const res = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
-export default Home
+    getData();
+  }, []);
+  return <div>This is my home</div>;
+};
+
+export default Home;
